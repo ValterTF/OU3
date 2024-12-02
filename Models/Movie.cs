aad4586d3829a26033834db68e32f3196f4ae84e
Valter <vath0008@ad.umu.se>
2024-12-02 18:51:18 +0100

    Added Movie class.

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace OU3.Models
+{
+    public class Movie
+    {
+        public Movie() { }
+
+        public int FilmID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public int CopiesAvailable { get; set; }
+    }
+}
