6d54b6cf3ce4e5b02677f0e718b222753719296e
Valter <vath0008@ad.umu.se>
2024-12-02 20:51:36 +0100

    First implementation of InsertMovie and View.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OU3.Models;
+
+namespace OU3.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult InsertMovie()
+        {
+            Movie movie = new Movie();
+            MovieMethods movieMethods = new MovieMethods();
+            int i = 0;
+            string error = "";
+
+            movie.Title = "New Movie";
+            movie.Year = 2024;
+            movie.Director = "ValterTF";
+            movie.CopiesAvailable = 1;
+
+            i = movieMethods.InsertMovie(movie, out error);
+            ViewBag.Error = error;
+            ViewBag.antal = i;
+
+            return View();
+        }
+    }
+}
