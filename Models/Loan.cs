05e460daddde1d7d98567fbf77453671e6097676
Valter <vath0008@ad.umu.se>
2024-12-05 11:23:25 +0100

    Added Loan table.

---
+++ Models/Loan.cs
@@ -0,0 +1,12 @@
+﻿namespace OU3.Models
+{
+    public class Loan
+    {
+        public Loan() { }
+        public int LoanID { get; set; }
+        public int FilmID { get; set; } 
+        public string BorrowerName { get; set; }
+        public DateTime LoanDate { get; set; } 
+        public DateTime? ReturnDate { get; set; }
+    }
+}
